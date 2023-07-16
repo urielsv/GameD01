e0990c332425d8b99af9bb20248a5bca6ea1b3ee
uriel <urielsosavazquez@gmail.com>
2023-07-16 18:37:33 -0300

    Fixes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -19,16 +19,25 @@ public class PlayerController : MonoBehaviour
 
     List<RaycastHit2D> _castCollisions = new List<RaycastHit2D>();
  
-    [SerializeField] private float moveSpeed = 5f;
-    // Start is called before the first frame update
+    [SerializeField] 
+    private float moveSpeed = 5f;
+    
+    private Vector3 lookDir;
+    private float lookAngle;
+
+    private Camera mainCamera;
     void Start()
     {
+        mainCamera = Camera.main;
         _rb = GetComponent<Rigidbody2D>(); 
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     { //If movement input is not 0, try to move
+        
+        FaceMousePosition();
+        
         if (_movementInput != Vector2.zero) {
             bool success = TryMove(_movementInput);
             if (!success && _movementInput.x > 0) {
@@ -76,5 +85,12 @@ public class PlayerController : MonoBehaviour
     
     void OnMove(InputValue movementValue) {
         _movementInput = movementValue.Get<Vector2>();
+    }
+
+    private void FaceMousePosition()
+    {
+        lookDir = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        lookAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; 
+        transform.rotation = Quaternion.Euler(1, 0, lookAngle);
     }
 }
